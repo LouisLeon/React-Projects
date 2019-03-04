ad6a7efc16c98a13f32c887b34b2d94af8df159f
LouisLeon <leon.louis14@gmail.com>
2019-03-04 11:01:25 -0800

    Updates

---
+++ precache-manifest.9ce53cef49d133f5385e6492e79b5ed8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "63e5a332d337eb7f07b4",
+    "url": "/React-tic-tac-toe/static/js/runtime~main.63e5a332.js"
+  },
+  {
+    "revision": "29b94f9851326a0044c9",
+    "url": "/React-tic-tac-toe/static/js/main.29b94f98.chunk.js"
+  },
+  {
+    "revision": "237b155b721a71109735",
+    "url": "/React-tic-tac-toe/static/js/2.237b155b.chunk.js"
+  },
+  {
+    "revision": "29b94f9851326a0044c9",
+    "url": "/React-tic-tac-toe/static/css/main.778b8cec.chunk.css"
+  },
+  {
+    "revision": "3b8d76808596bcb6565c4de30c39176c",
+    "url": "/React-tic-tac-toe/index.html"
+  }
+];
